2792a6e518a2266e4754876be3bfc06cf29230a6
kandyula akhil <akhilkandayala@gmail.com>
2025-07-10 19:33:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = new pg.Client({
-user: "postgres",
-host: "localhost",
-database: "akhil",
-password: "1992",
-port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
